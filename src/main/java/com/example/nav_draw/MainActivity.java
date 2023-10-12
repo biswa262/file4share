4416c7d3c44004ea328b73976166141dcf113406
biswa262 <147683663+biswa262@users.noreply.github.com>
2023-10-12 11:16:51 +0530

    Add files via upload

---
+++ src/main/java/com/example/nav_draw/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.nav_draw;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.Toolbar;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
